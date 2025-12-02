b734de359f4aeb88f5790e4020883ac1f8beb2cc
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-12-02 14:19:14 +0100

    Add Opportunity model with associations

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,6 +1,7 @@
 class Statement < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :opportunities, dependent: :destroy
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :date, presence: true
