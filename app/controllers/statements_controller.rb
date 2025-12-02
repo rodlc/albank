365422549a67fd1f526e406a74bc526cf17b06bb
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-12-02 13:55:34 +0100

    Add StatementsController

---
+++ app/controllers/statements_controller.rb
@@ -0,0 +1,9 @@
+class StatementsController < ApplicationController
+  def index
+    @statements = current_user.statements
+  end
+
+  def show
+    @statement = current_user.statements.find(params[:id])
+  end
+end
