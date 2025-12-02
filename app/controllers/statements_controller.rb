badb57ebf12c8bdc0184f6a8460208ef888def24
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-12-02 16:59:35 +0100

    Update StatementsController: add new/create actions and load expenses

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,9 +1,31 @@
 class StatementsController < ApplicationController
   def index
-    @statements = current_user.statements
+    @statements = current_user.statements.order(date: :desc)
   end
 
   def show
     @statement = current_user.statements.find(params[:id])
+    @expenses = @statement.expenses.includes(:category, :opportunities)
+  end
+
+  def new
+    @statement = current_user.statements.build
+  end
+
+  def create
+    @statement = current_user.statements.build(statement_params)
+
+    if @statement.save
+      # TODO: Parse PDF and create expenses
+      redirect_to @statement, notice: "Statement was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def statement_params
+    params.require(:statement).permit(:date)
   end
 end
