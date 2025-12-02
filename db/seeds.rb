05b4a34c06a1df70313793819532f13a9193faa1
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-12-02 13:51:57 +0100

    Add Category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning categories..."
+Category.destroy_all
+
+puts "Creating categories..."
+Category.create!(name: "Assurance Auto", keywords: "auto voiture")
+Category.create!(name: "Assurance Habitation", keywords: "habitation logement")
+Category.create!(name: "Électricité & Gaz", keywords: "énergie électricité gaz")
+Category.create!(name: "Box Internet", keywords: "internet box fibre")
+Category.create!(name: "Banque", keywords: "frais bancaires")
+
+puts "✓ #{Category.count} categories created"
